e9f81c34fd098622aa7cd439de1d7d7d24e1f351
wszjdjj-netizen <wszjdjj-netizen@users.noreply.github.com>
2026-06-09 17:41:56 +0800

    Switch to hash router to fix SPA 404 on static hosting

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 
 const routes = [
@@ -89,7 +89,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
